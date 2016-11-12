0a128cbedaba2564d1bf1f56dd5878aa0b8264a0
Robin <victorino.robin@gmail.com>
2016-11-12 15:44:24 +0100

    :)

--- pbg.js
+++ pbg.js
@@ -36,25 +36,46 @@ class Pbg {
       throw new ReferenceError("fabric is not defined. Did you include fabric.js ?");
     }
   }
+
+  play(){
+    this.actions.forEach(function(action){
+      action.play();
+    });
+  }
 }
 
 class AbstractAction {
-  constructor(){
+  constructor(canvas){
     if(new.target === AbstractAction){
       throw new TypeError("Cannot construct abstract instance: AbstractAction");
     }
+    this.canvas = canvas;
+  }
+  play(){}
+}
+
+class IntroAction extends AbstractAction {
+  constructor(c){
+    super(c);
+    this.player = new fabric.Image.fromURL('images/player.png');
+  }
+  play(){
+    this.init();
+  }
+  init(){
+    this.canvas.add(this.player);
   }
 }
 
 class AttackAction extends AbstractAction {
-  constructor() {
-    super();
+  constructor(c) {
+    super(c);
   }
 }
 
 class UseObjectAction extends AbstractAction {
-  constructor() {
-    super();
+  constructor(c) {
+    super(c);
 
   }
 }
@@ -79,12 +100,6 @@ class Player {
     this.name = "";
     this.health = 10;
     this.maxHealth = 10;
-  }
-
-  constructor(name, avatar){
-    this.avatar = new Image(300,300);
-    this.avatar.src = avatar;
-    this.name = name;
   }
 
   setAvatar(avatar){
