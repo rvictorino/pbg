ce38d2a6cb6694affe2906e9edd78d2f6ab864c3
VICTORINO Robin <VICTORINO@dev.groupe.adg>
2016-12-26 16:58:57 +0100

    added Intro animation

--- pbg.js
+++ pbg.js
@@ -38,6 +38,7 @@ class Pbg {
     if(typeof fabric !== 'undefined'){
       var canvas = this.canvas = new fabric.Canvas(canvasID);
       fabric.Object.prototype.transparentCorners = false;
+      canvas.select = false;
     } else {
       throw new ReferenceError("fabric is not defined. Did you include fabric.js ?");
     }
@@ -69,15 +70,33 @@ class AbstractAction {
 }
 
 class IntroAction extends AbstractAction {
-  constructor(c){
+  constructor(c, player, opponent){
     super(c);
-
+    this.player = player;
+    this.opponent = opponent;
   }
   play(){
-    this.init();
-  }
-  init(){
-    this.canvas.add(this.player);
+    var canvas = this.canvas;
+    fabric.Image.fromURL(this.opponent.avatar, function(img){
+      img.set(IMG_DEFAULT_OPT);
+      img.scaleToWidth(canvas.getWidth()/5);
+      img.set({
+        left: - img.getWidth(),
+        top: 0
+      });
+      canvas.add(img);
+      img.animate('left', '+='+ canvas.getWidth());
+    });
+    fabric.Image.fromURL(this.player.avatar, function(img){
+      img.set(IMG_DEFAULT_OPT);
+      img.scaleToWidth(canvas.getWidth()/3);
+      img.set({
+        left: canvas.getWidth(),
+        top: canvas.getHeight() - 350
+      });
+      canvas.add(img);
+      img.animate('left', '-='+ canvas.getWidth());
+    });
   }
 }
 
@@ -85,7 +104,7 @@ class IntroAction extends AbstractAction {
 class TextAction extends AbstractAction {
     constructor(c, fullText) {
       super(c);
-      this.fullText = "abcypq,'";
+      this.fullText = fullText;
       this.textObject = [];
       this.startImageId = 1;
       this.endImageId = 1;
@@ -101,6 +120,7 @@ class TextAction extends AbstractAction {
 
       new fabric.Image.fromURL("sprites/full_panel.png", function(img) {
         img.scaleToWidth(canvas.getWidth());
+        img.set(IMG_DEFAULT_OPT);
         img.set({
           left: 0,
           top: canvas.getHeight() - img.getHeight(),
@@ -141,7 +161,7 @@ class TextAction extends AbstractAction {
     }
 
     drawLetter(canvas, letter, line, pos, time){
-      var letterImgPath = this.getImagePathFromChar(letter);
+      var letterImgPath = PbgHelper.getImagePathFromChar(letter);
       var topPos = canvas.getHeight()-(canvas.getHeight()/(4*line));
       topPos+=(["p","g","q","y","j"].indexOf(letter) >= 0 ? 1 : 0)*(canvas.getHeight()/140);
       topPos+=([","].indexOf(letter) >= 0 ? 3 : 0)*(canvas.getHeight()/140);
@@ -150,6 +170,7 @@ class TextAction extends AbstractAction {
       setTimeout(function(){
       self.textObject.push( new fabric.Image.fromURL(letterImgPath, function(img) {
         img.scaleToWidth((canvas.getWidth()/18));
+        img.set(IMG_DEFAULT_OPT);
         canvas.add(img);
       },{
         top: topPos,
@@ -171,6 +192,7 @@ class TextAction extends AbstractAction {
       setTimeout(function(){
         fabric.Image.fromURL("sprites/letter/space.png", function(img) {
           img.scaleToWidth((canvas.getWidth()/18));
+          img.set(IMG_DEFAULT_OPT);
           canvas.add(img);
         },{
           top: canvas.getHeight()-(canvas.getHeight()/(4*line)),
@@ -187,152 +209,7 @@ class TextAction extends AbstractAction {
       },time);
     }
 
-    getImagePathFromChar(char){
-      switch (char) {
-          case "a":
-            return 'sprites/letter_min/a.png';
-          case "b":
-            return 'sprites/letter_min/b.png';
-          case "c":
-            return 'sprites/letter_min/c.png';
-          case "d":
-            return 'sprites/letter_min/d.png';
-          case "e":
-            return 'sprites/letter_min/e.png';
-          case "f":
-            return 'sprites/letter_min/f.png';
-          case "g":
-            return 'sprites/letter_min/g.png';
-          case "h":
-            return 'sprites/letter_min/h.png';
-          case "i":
-            return 'sprites/letter_min/i.png';
-          case "j":
-            return 'sprites/letter_min/j.png';
-          case "k":
-            return 'sprites/letter_min/k.png';
-          case "l":
-            return 'sprites/letter_min/l.png';
-          case "m":
-            return 'sprites/letter_min/m.png';
-          case "n":
-            return 'sprites/letter_min/n.png';
-          case "o":
-            return 'sprites/letter_min/o.png';
-          case "p":
-            return 'sprites/letter_min/p.png';
-          case "q":
-            return 'sprites/letter_min/q.png';
-          case "r":
-            return 'sprites/letter_min/r.png';
-          case "s":
-            return 'sprites/letter_min/s.png';
-          case "t":
-            return 'sprites/letter_min/t.png';
-          case "u":
-            return 'sprites/letter_min/u.png';
-          case "v":
-            return 'sprites/letter_min/v.png';
-          case "w":
-            return 'sprites/letter_min/w.png';
-          case "x":
-            return 'sprites/letter_min/x.png';
-          case "y":
-            return 'sprites/letter_min/y.png';
-          case "z":
-            return 'sprites/letter_min/z.png';
-          case "A":
-            return 'sprites/letter/a.png';
-          case "B":
-            return 'sprites/letter/b.png';
-          case "C":
-            return 'sprites/letter/c.png';
-          case "D":
-            return 'sprites/letter/d.png';
-          case "E":
-            return 'sprites/letter/e.png';
-          case "F":
-            return 'sprites/letter/f.png';
-          case "G":
-            return 'sprites/letter/g.png';
-          case "H":
-            return 'sprites/letter/h.png';
-          case "I":
-            return 'sprites/letter/i.png';
-          case "J":
-            return 'sprites/letter/j.png';
-          case "K":
-            return 'sprites/letter/k.png';
-          case "L":
-            return 'sprites/letter/l.png';
-          case "M":
-            return 'sprites/letter/m.png';
-          case "N":
-            return 'sprites/letter/n.png';
-          case "O":
-            return 'sprites/letter/o.png';
-          case "P":
-            return 'sprites/letter/p.png';
-          case "Q":
-            return 'sprites/letter/q.png';
-          case "R":
-            return 'sprites/letter/r.png';
-          case "S":
-            return 'sprites/letter/s.png';
-          case "T":
-            return 'sprites/letter/t.png';
-          case "U":
-            return 'sprites/letter/u.png';
-          case "V":
-            return 'sprites/letter/v.png';
-          case "W":
-            return 'sprites/letter/w.png';
-          case "X":
-            return 'sprites/letter/x.png';
-          case "Y":
-            return 'sprites/letter/y.png';
-          case "Z":
-            return 'sprites/letter/z.png';
-          case ".":
-            return 'sprites/letter/period.png';
-          case ",":
-            return 'sprites/letter/comma.png';
-          case "'":
-            return 'sprites/letter/comma.png';
-          case ":":
-            return 'sprites/letter/colon.png';
-          case ";":
-            return 'sprites/letter/semicolon.png';
-          case "(":
-            return 'sprites/letter/(.png';
-          case ")":
-            return 'sprites/letter/).png';
-          case "-":
-            return 'sprites/letter/-.png';
-          case "[":
-            return 'sprites/letter/[.png';
-          case "]":
-            return 'sprites/letter/].png';
-          case "!":
-            return 'sprites/letter/!.png';
-          case "/":
-            return 'sprites/letter/slash.png';
-          case " ":
-            return 'sprites/letter/space.png';
-          case "§":
-            return 'sprites/letter/pk.png';
-          case "%":
-            return 'sprites/letter/mn.png';
-          case "*":
-            return 'sprites/letter/ok.png';
-          case "=":
-            return 'sprites/letter/male.png';
-          case "+":
-            return 'sprites/letter/female.png';
-          default:
-            return 'sprites/letter/int.png';
-        }
-    }
+
     sleep(delay) {
       var start = new Date().getTime();
       while (new Date().getTime() < start + delay){}
@@ -436,4 +313,157 @@ class Player {
   getMoves(){
     return this.moves;
   }
+}
+
+class PbgHelper {
+
+  static getImagePathFromChar(char){
+    switch (char) {
+        case "a":
+          return 'sprites/letter_min/a.png';
+        case "b":
+          return 'sprites/letter_min/b.png';
+        case "c":
+          return 'sprites/letter_min/c.png';
+        case "d":
+          return 'sprites/letter_min/d.png';
+        case "e":
+          return 'sprites/letter_min/e.png';
+        case "f":
+          return 'sprites/letter_min/f.png';
+        case "g":
+          return 'sprites/letter_min/g.png';
+        case "h":
+          return 'sprites/letter_min/h.png';
+        case "i":
+          return 'sprites/letter_min/i.png';
+        case "j":
+          return 'sprites/letter_min/j.png';
+        case "k":
+          return 'sprites/letter_min/k.png';
+        case "l":
+          return 'sprites/letter_min/l.png';
+        case "m":
+          return 'sprites/letter_min/m.png';
+        case "n":
+          return 'sprites/letter_min/n.png';
+        case "o":
+          return 'sprites/letter_min/o.png';
+        case "p":
+          return 'sprites/letter_min/p.png';
+        case "q":
+          return 'sprites/letter_min/q.png';
+        case "r":
+          return 'sprites/letter_min/r.png';
+        case "s":
+          return 'sprites/letter_min/s.png';
+        case "t":
+          return 'sprites/letter_min/t.png';
+        case "u":
+          return 'sprites/letter_min/u.png';
+        case "v":
+          return 'sprites/letter_min/v.png';
+        case "w":
+          return 'sprites/letter_min/w.png';
+        case "x":
+          return 'sprites/letter_min/x.png';
+        case "y":
+          return 'sprites/letter_min/y.png';
+        case "z":
+          return 'sprites/letter_min/z.png';
+        case "A":
+          return 'sprites/letter/a.png';
+        case "B":
+          return 'sprites/letter/b.png';
+        case "C":
+          return 'sprites/letter/c.png';
+        case "D":
+          return 'sprites/letter/d.png';
+        case "E":
+          return 'sprites/letter/e.png';
+        case "F":
+          return 'sprites/letter/f.png';
+        case "G":
+          return 'sprites/letter/g.png';
+        case "H":
+          return 'sprites/letter/h.png';
+        case "I":
+          return 'sprites/letter/i.png';
+        case "J":
+          return 'sprites/letter/j.png';
+        case "K":
+          return 'sprites/letter/k.png';
+        case "L":
+          return 'sprites/letter/l.png';
+        case "M":
+          return 'sprites/letter/m.png';
+        case "N":
+          return 'sprites/letter/n.png';
+        case "O":
+          return 'sprites/letter/o.png';
+        case "P":
+          return 'sprites/letter/p.png';
+        case "Q":
+          return 'sprites/letter/q.png';
+        case "R":
+          return 'sprites/letter/r.png';
+        case "S":
+          return 'sprites/letter/s.png';
+        case "T":
+          return 'sprites/letter/t.png';
+        case "U":
+          return 'sprites/letter/u.png';
+        case "V":
+          return 'sprites/letter/v.png';
+        case "W":
+          return 'sprites/letter/w.png';
+        case "X":
+          return 'sprites/letter/x.png';
+        case "Y":
+          return 'sprites/letter/y.png';
+        case "Z":
+          return 'sprites/letter/z.png';
+        case ".":
+          return 'sprites/letter/period.png';
+        case ",":
+          return 'sprites/letter/comma.png';
+        case "'":
+          return 'sprites/letter/comma.png';
+        case ":":
+          return 'sprites/letter/colon.png';
+        case ";":
+          return 'sprites/letter/semicolon.png';
+        case "(":
+          return 'sprites/letter/(.png';
+        case ")":
+          return 'sprites/letter/).png';
+        case "-":
+          return 'sprites/letter/-.png';
+        case "[":
+          return 'sprites/letter/[.png';
+        case "]":
+          return 'sprites/letter/].png';
+        case "!":
+          return 'sprites/letter/!.png';
+        case "/":
+          return 'sprites/letter/slash.png';
+        case " ":
+          return 'sprites/letter/space.png';
+        case "§":
+          return 'sprites/letter/pk.png';
+        case "%":
+          return 'sprites/letter/mn.png';
+        case "*":
+          return 'sprites/letter/ok.png';
+        case "=":
+          return 'sprites/letter/male.png';
+        case "+":
+          return 'sprites/letter/female.png';
+        default:
+          return 'sprites/letter/int.png';
+      }
+  }
+}
+IMG_DEFAULT_OPT = {
+  selectable: false
 }
